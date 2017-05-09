7756fcbcccf4d46e54c6de3f5e16d5a53dcb01ff
manmolecular <regwebghost@yandex.ru>
2017-05-09 13:46:56 +0700

    Revert "Revert "Gjbc""
    This reverts commit c7d27b36caf9a5594820eba750c080a8c4458fdb.

--- Dots/Source.cpp
+++ Dots/Source.cpp
@@ -15,7 +15,7 @@ int dots::rand_border = 0;
 int hash_table::_table_size = _size;
 const int field::f_size = _size;
 
-void hash_search()
+void hash_search(dots _temp)
 {
 	/* Начальные объявления */
 	dots temp;
@@ -29,7 +29,6 @@ void hash_search()
 	my_table.get_random_table();
 
 	/* Добавление в таблицу */
-	temp.randomize(42);
 	my_table.push(temp);
 	my_table.push(temp);
 	if (_show)
@@ -65,7 +64,8 @@ int main()
 	clock_t start, end;
 
 	/* Поиск точки в пространстве */
-	dots temp = my_field.return_dot(5);
+	srand(time(NULL));
+	dots temp = my_field.return_dot(rand() % _size);
 	int _pos = 0;
 	start = clock();
 	cout << endl << boolalpha << "Search: " << my_field.search(temp, &_pos) << endl;
@@ -76,12 +76,12 @@ int main()
 	cout << "Runtime: " << ((double)end - start) / ((double)CLOCKS_PER_SEC) << "s" << endl;
 
 	/* Поиск для 100к элементов */
-	hash_search();
+	hash_search(temp);
 
 	hash_table::set_size(_size * 2);
 
 	/* Поиск для 200к элементов */
-	hash_search();
+	hash_search(temp);
 
 	return 0;
 }
